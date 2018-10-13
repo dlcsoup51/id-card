703e78237b525af2a0effc7284e4260a647d3896
Donna L Campbell <dlcsoup51@me.com>
2018-10-13 14:05:06 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function idCard (){
+    var name="firstName";
+    
+    var firstName=document.getElementById('First Name'.value);
+    var lastName=document.getElementById('Last Name'.value);
+    var address=document.getElementById('Address'.value);
+    var postFullName=document.getElementByTagName('firstName','lastName');
+    var postAddress=document.getElementByTagName('Address')
+    var age=parseInt(document.getElementById('Age'));
+    var phoneNumber=parseInt(document.getElementById('Phone Number'));
+}
